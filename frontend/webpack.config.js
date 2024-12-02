18a5a8611eecbae1173c3c4832e3ac1926b4f532
devaknyu <ds8095@nyu.edu>
2024-12-02 07:28:11 +0400

    Push dist folder and Webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/app.js', // Entry point for your application
+  entry: './app.js', // Correct path to app.js inside the 'frontend' folder
   output: {
     filename: 'bundle.js', // Output bundled file
     path: path.resolve(__dirname, 'dist'), // Output directory
